a9b60b5f3d847f2a75afa1baf9c40958dab562f0
Ariel Cuesta <arielcc88@gmail.com>
2020-09-21 15:27:11 -0500

    react router integrated. Home page placeholder component created and render correctly.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { link } from "react-router-dom";
+
+//Home React Component -- Home Page
+function Home() {
+  return (
+    <div className="App">
+      <h2>Welcome to POD E-Learning App</h2>
+      <p>Application under construction.</p>
+    </div>
+  );
+}
+
+export default Home;
