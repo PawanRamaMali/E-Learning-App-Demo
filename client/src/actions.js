8d560829c6b5e84b3a3f3a68cdd71909a725fd4c
Carmen Johnson <carmenjohnson512@gmail.com>
2020-09-21 21:34:27 -0500

    added baseline component files and redux files/dependencies

---
+++ client/src/actions.js
@@ -0,0 +1,11 @@
+import { ADD_USER, DELETE_USER } from './constants';
+
+export const addUser = (user) => ({
+  type: ADD_USER,
+  payload: user,
+});
+
+export const deleteUser = (id) => ({
+  type: DELETE_USER,
+  payload: id,
+});
---
+++ client/src/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer, { initialState } from './reducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+  reducer,
+  initialState,
+  compose(applyMiddleware(thunk), composeWithDevTools())
+);
+
+export default store
