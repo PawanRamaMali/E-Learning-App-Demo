14817ec068f7521791cce7853fbc70e3faee8051
Samir Bello <agnide4@gmail.com>
2020-09-23 03:41:33 -0500

    Nav bar complete/Moving onto page body

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 //Importing pages
 import Home from "./pages/Home";
 import { Route, Switch } from "react-router-dom";
+import instructorHeader from "./Components/instructorHeader";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/instructor" component={instructorHeader} />
       </Switch>
     </div>
   );
