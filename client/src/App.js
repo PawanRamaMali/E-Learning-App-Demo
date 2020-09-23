6d102c03869ddeb02e2c40ca73dc3ebb32c9c898
Samir Bello <agnide4@gmail.com>
2020-09-23 11:08:59 -0500

    Instructor Landing created

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import "./App.css";
 //Importing pages
 import Home from "./pages/Home";
 import { Route, Switch } from "react-router-dom";
-import instructorHeader from "./Components/instructorHeader";
+
+import InsLanding from "./pages/InsLanding";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/instructor" component={instructorHeader} />
+        <Route exact path="/instructor" component={InsLanding} />
       </Switch>
     </div>
   );
---
+++ client/src/pages/InsLanding.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {useState, useEffect} from "react"
+import {Jumbotron, Button } from "react-bootstrap"
+import InstructorHeader from "../Components/instructorHeader";
+
+
+export default function InsLanding() {
+    const [AddStu, setStudent] = useState();
+    const [AddCourses, setCourses] = useState(false);
+
+
+    return (
+
+        
+        <React.Fragment> 
+            <InstructorHeader />
+            <Jumbotron className="InsLanding-background">
+            <div className="InsLanding-content">
+                <h1 className="">POD | E-Learning</h1>
+                <p>
+                    Feeding the minds one video at a time.
+                </p>
+                <p className="btngroup">
+                    <Button className="InsBtn AddStu primary-button">ADD STUDENT</Button>{' '}
+                    <Button className="InsBtn AddCourses primary-button">ADD COURSES</Button>{' '}
+                    <Button className="InsBtn Dashboard primary-button">VIEW DASHBOARD</Button>{' '}
+                </p>
+            </div>
+        </Jumbotron>
+        </React.Fragment>
+        
+        
+
+
+    )
+}
